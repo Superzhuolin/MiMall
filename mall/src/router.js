50fee3478d485c80589602545be1b95a735ddc53
Superzhuolin <1910185040@qq.com>
2022-08-27 23:16:19 +0800

    1

--- mall/src/router.js
+++ mall/src/router.js
@@ -20,6 +20,7 @@ export default new Router({
             path: '/',
             name: "home",
             component: Home,
+            redirect: "/index",
             chiledren: [
                 {
                     path: '/index',
