ce225d99795db989db52de6d59cdb2b35ec4bb9c
Superzhuolin <1910185040@qq.com>
2022-08-27 23:07:17 +0800

    11

--- mall/src/pages/home.vue
+++ mall/src/pages/home.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        <nav-header></nav-header>
-        <router-view></router-view>
-        <nav-footer></nav-footer>
-    </div>    
+  <div>
+    home
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
 </template>
+<script>
+import NavHeader from "./../components/NavFooter.vue";
+import NavFooter from "./../components/NavHeade.vue";
+export default {
+  name: "nav-home",
+  components: {
+    NavHeader,
+    NavFooter,
+  },
+};
+</script>
--- mall/src/router.js
+++ mall/src/router.js
@@ -1,5 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
+import Home from "./pages/home.vue"
+import Index from "./pages/index.vue"
 import Product from './pages/product.vue'
 import Detail from './pages/detail.vue'
 import Cart from './pages/cart'
@@ -18,6 +20,7 @@ export default new Router({
             path: '/',
             name: "home",
             component: Home,
+            // redirect: "index",
             chiledren: [
                 {
                     path: '/index',
