50f2649bf80d1c69967b3b6a4a35dcc5dcf4c5e7
Superzhuolin <1910185040@qq.com>
2022-08-29 12:14:55 +0800

    east-mock完成

--- mall/src/main.js
+++ mall/src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'  //主键
 //import env from "./env"  //  ./表示当前目录 若没有会被当成插件
 
 //mock开关
-const mock = true;
+const mock = false;
 if(mock){
   require("./mock/api");
 }
@@ -14,7 +14,8 @@ if(mock){
 // 为发请求时设置基础值
 //根据前端的跨域方式做调整  (接口代理  前端域名跟接口域名是一样的)
  // /a/b  :/api/a/b=>/a/b
-axios.defaults.baseURL = "/api"; 
+axios.defaults.baseURL = " https://mock.mengxuegu.com/mock/630c3a1bc94098684fb183bd/api"; 
+// axios.defaults.baseURL = " /"; 
 axios.defaults.timeout = 8000;  //设置超出时间
 //根据环境变量获取不同的请求地址
 // axios.defaults.baseURL = env.baseURL;
