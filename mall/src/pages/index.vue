10818fbfd64cb44f2b1898c17fd8966cb7433cd4
Superzhuolin <1910185040@qq.com>
2022-09-01 09:06:55 +0800

    导航菜单完成

--- mall/src/pages/index.vue
+++ mall/src/pages/index.vue
@@ -1,8 +1,64 @@
 <template>
   <div class="index">
     <div class="container">
-      <!-- 轮播界面 -->
       <div class="swiper-box">
+        <!-- 导航菜单 -->
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡 </a>
+              <div class="children">
+                <ul v-for="(item, i) in menuList" :key="i">
+                  <li v-for="(sub, j) in item" :key="j">
+                    <a
+                      :href="sub ? '/#/product/' + sub.id : ''"
+                      target="_blank"
+                    >
+                      <img
+                        :src="sub ? sub.img : '/imgs/item-box-1.png'"
+                        alt="加载失败"
+                      />
+                      {{ sub ? sub.name : "小米9" }}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+              <div class="children"></div>
+            </li>
+          </ul>
+        </div>
+        <!-- 轮播界面 -->
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item, index) in swiperList" :key="index">
             <a :href="'/#/product/' + item.id"><img :src="item.img" /></a>
@@ -75,16 +131,119 @@ export default {
           id: "",
           img: "/imgs/slider/slide-5.jpg",
         },
+      ], //菜单表
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9",
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版",
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro",
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区",
+          },
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
       ],
     };
   },
 };
 </script>
 <style lang="scss">
+@import "./../assets/scss/mixin.scss";
+@import "./../assets/scss/config.scss";
 .index {
   .swiper-box {
+    .nav-menu {
+      // opacity: 0.7;//使用opacity会让字体也会变得透明
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      padding: 26px 0;
+      background-color: #55585a7a;
+      box-sizing: border-box; //定义盒子模型(保持尺寸)
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px; //设置行高
+          a {
+            position: relative;
+            display: block;
+            font-size: 16px;
+            color: #ffffff;
+            padding-left: 30px;
+            &:after {
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              content: " ";
+              //箭头无法显示
+              @include bgImg(10px, 10px, "/public/imgs/icon-arrow.png");
+            }
+          }
+          &:hover {
+            background-color: $colorA;
+            .children {
+              display: block;
+            }
+          }
+          .children {
+            display: none; //默认不给看见
+            width: 952px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid $colorH;
+            ul {
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li {
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                // width: 241px;
+                padding-left: 23px;
+              }
+              a {
+                color: $colorB;
+                font-size: 14px;
+              }
+              img {
+                width: 42px;
+                height: 35px;
+                vertical-align: middle; //图片和文字居中
+                margin-right: 15px;
+              }
+            }
+          }
+        }
+      }
+    }
     .swiper-container {
       height: 451px;
+      .swiper-button-prev {
+        left: 274px; //改变右导航菜单位置
+      }
       img {
         width: 100%;
         height: 100%;
