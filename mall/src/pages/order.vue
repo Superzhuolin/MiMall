7e39a1337ddfbf11ce930ba03ec4f9193174e609
Superzhuolin <1910185040@qq.com>
2022-09-05 08:30:35 +0800

    12-1订单父组件结构封装

--- mall/src/pages/order.vue
+++ mall/src/pages/order.vue
@@ -1,20 +1,47 @@
 <template>
   <div>
-    <order-header></order-header>
+    <!-- 购物车头部组件 -->
+    <order-header :title="title">
+      <template v-slot:tip>
+        <span>{{tip}}</span>
+      </template>
+    </order-header>
     <router-view></router-view>
+    <service-bar></service-bar>
     <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
 import OrderHeader from "./../components/OrderHeader.vue";
+import ServiceBar from "./../components/ServiceBar.vue";
 import NavFooter from "./../components/NavFooter.vue";
 
 export default {
   name: "order",
+  data(){
+    return{
+      title:"",
+      tip:""
+    }
+  },
   components: {
     OrderHeader,
+    ServiceBar,
     NavFooter,
   },
+  mounted(){
+    let path = this.$route.path; //获取当前路由地址
+    if(path == '/order/confirm'){
+      this.title = "订单确认";
+      this.tip="请认真填写收获地址";
+    }else if (path == '/order/list'){
+      this.title = "订单列表";
+      this.tip="请谨防钓鱼链接或诈骗电话,了解更多>";
+    }else if (path == '/order/pay'){
+      this.title = "订单支付";
+      this.tip="请谨防钓鱼链接或诈骗电话,了解更多>";
+    }
+  }
 };
 </script>
