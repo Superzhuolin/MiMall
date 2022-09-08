56629fdf681b3dc1c69a574c1e0d21396f294fee
Superzhuolin <1910185040@qq.com>
2022-09-08 17:21:58 +0800

    15-2config.js更改

--- mall/vue.config.js
+++ mall/vue.config.js
@@ -1,12 +1,13 @@
 //webpack的配置表  会传送给node.js服务器从而实现服务的设置
+/* 项目是通过vue Cli脚手架打包构建的  所有关于项目的配置都在vue.config,js中设置的 */
 const { defineConfig } = require('@vue/cli-service')
 // 导入模块
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: true,// 是否将所有文件都编译一遍（通过 babel 编译文件：ES6 + → ES5）
   lintOnSave: false,
-  devServer: {
-    host: "localhost",
-    port: 8080,  //你好
+  devServer: {//devServer是本地开发服务器的配置信息.
+    host: "localhost", //指定本地访问主机
+    port: 8080,  //端口
     proxy: { //代理
       '/api': {
         target: 'http://mall-pre.springboot.cn', 
@@ -18,7 +19,13 @@ module.exports = defineConfig({
       }
     }
   },
-  // lintOnSave:false//关闭Eslink校验
+  // publicPath:'/app',  //设置前缀
+  // outputDir:'dist',  //改变压缩包的文件名
+  // indexPath:'index2.html',  //改变index.html的文件名
+  // productionSourceMap: true,//生产环境的source map  设置后网页不可查看源代码
+  // chainWebpack: (config) => {
+  //   config.plugins.delete('prefetch');
+  // }
 })
 
 /* 
