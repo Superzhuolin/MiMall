83bd248e8263f92e79f8d1f93c46eac28179456c
Superzhuolin <1910185040@qq.com>
2022-09-08 19:00:57 +0800

    bug修复

--- mall/src/router.js
+++ mall/src/router.js
@@ -6,9 +6,9 @@ import Index from "./pages/index.vue"
 // import Product from './pages/product.vue'
 // import Detail from './pages/detail.vue'
 import Cart from './pages/cart'
-import Order from './pages/order.vue'
+// import Order from './pages/order.vue'
 import OrderConfirm from './pages/orderConfirm.vue'
-import OrderList from './pages/orderList.vue'
+// import OrderList from './pages/orderList.vue'
 import OrderPay from './pages/orderPay.vue'
 import AliPay from './pages/alipay.vue'
 // import { resolve } from "core-js/fn/promise"
@@ -34,12 +34,14 @@ export default new Router({
                     // component: Product,
                     // 通过require方式加载页面主键,中间数组传入要加载的页面,外面用resolve抛出
                     // 从而实现产品站的按需加载
-                    component: resolve=>require(["./pages/index.vue"],resolve),
+                    // component: resolve=>require(["./pages/index.vue"],resolve),
+                    component: () => import('./pages/product.vue')
                 }, {
                     path: '/detail/:id',
                     name: "detail",
                     // component: Detail,
-                    component: resolve => require(["./pages/detail.vue"], resolve),
+                    // component: resolve => require(["./pages/detail.vue"], resolve),
+                    component: () => import('./pages/detail.vue')
                 },
             ]
         },
@@ -47,23 +49,26 @@ export default new Router({
             path: '/login',
             name: "login",
             // component: Login,
-            component: resolve => require(["./pages/login.vue"], resolve),
-
+            // component: resolve => require(["./pages/login.vue"], resolve),
+            component: () => import('./pages/login.vue')
         },
         {
             path: '/cart',
             name: "cart",
-            component: Cart,
+            // component: Cart,
+            component: () => import('./pages/cart.vue')
         },
         {
             path: '/order',
             name: "order",
-            component: Order,
+            // component: Order,
+            component: () => import('./pages/order.vue'),
             children: [
                 {
                     path: 'list',
                     name: "order-list",
-                    component: OrderList,
+                    // component: OrderList,
+                    component: () => import("./pages/orderList.vue"),
                 }, {
                     path: 'confirm',
                     name: "order-confirm",
